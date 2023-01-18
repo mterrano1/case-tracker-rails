90eeab629d6e6f42fc1350c4fc7b5fa2f0f9d766
Christopher Terrano <mterrano1@yahoo.com>
2023-01-18 11:36:25 -0600

    create datetime method and include datehelper to calculate how many days ago a case was created

--- app/serializers/case_serializer.rb
+++ app/serializers/case_serializer.rb
@@ -1,8 +1,14 @@
 class CaseSerializer < ActiveModel::Serializer
-  attributes :id, :allegation_type, :allegation, :department, :status, :resolution, :datetime 
+  include ActionView::Helpers::DateHelper
+
+  attributes :id, :allegation_type, :allegation, :department, :status, :resolution, :datetime, :days_ago 
 
   def datetime
     created_at = self.object.created_at
     datetime = created_at.strftime("%B %d, %Y %I:%M %p")
+  end
+
+  def days_ago
+    object.created_at ? "#{distance_of_time_in_words(Time.now, object.created_at)} ago" : nil
   end
 end
