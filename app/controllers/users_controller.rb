99707d95888806b5b48c2e364c7418cadf3b8131
Christopher Terrano <mterrano1@yahoo.com>
2023-01-18 11:14:08 -0600

    create sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    
+    #POST /signup
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    #GET /me
+    def show
+        render json: @current_user
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :role, :username, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  get '/me', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
 end
