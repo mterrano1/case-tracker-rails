5ec5bf689fea8a53d6eaae8518f48b5f3317c516
Christopher Terrano <mterrano1@yahoo.com>
2023-01-18 11:38:59 -0600

    create new user serializer methods to count cases according to status

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :role
+  attributes :id, :first_name, :last_name, :role, :open_case_count, :closed_case_count, :unassigned_case_count
   has_many :assigned_cases
+
+  def open_case_count
+    object.assigned_cases.joins(:case_assignments).where(cases: { status: "Open" }).size
+  end
+
+  def closed_case_count
+    object.assigned_cases.joins(:case_assignments).where(cases: { status: "Closed" }).size
+  end
+
+  def unassigned_case_count
+    object.assigned_cases.joins(:case_assignments).where(cases: { status: "Unassigned" }).size
+  end
 end
