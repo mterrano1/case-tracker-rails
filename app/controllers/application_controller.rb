119454216dc85f23ddc894895beff6563ee55a37
Christopher Terrano <mterrano1@yahoo.com>
2023-01-18 11:15:24 -0600

    create auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
     before_action :authorize
 
     private
