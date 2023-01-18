a3bc51966a876f6f41628ea9fe6ef0b31540eb8b
Christopher Terrano <mterrano1@yahoo.com>
2023-01-18 11:17:09 -0600

    create validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::API
 
     before_action :authorize
 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     private
 
     def authorize
         @current_user = User.find_by(id: session[:user_id])
 
         render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
+
+    def render_unprocessable_entity_response(e)
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-    
+    skip_before_action :authorize, only: :create
+
     #POST /signup
     def create
         user = User.create!(user_params)
+        session[:user_id] = user.id
         render json: user, status: :created
     end
 
